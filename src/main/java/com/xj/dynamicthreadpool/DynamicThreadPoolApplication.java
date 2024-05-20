58b125afcaeb92d4cdfbe65b71681edbe8b73507
tiannoyi <tiannoyi@qq.com>
2024-05-20 10:28:51 +0800

    init

---
+++ src/main/java/com/xj/dynamicthreadpool/DynamicThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.xj.dynamicthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicThreadPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicThreadPoolApplication.class, args);
+    }
+
+}
