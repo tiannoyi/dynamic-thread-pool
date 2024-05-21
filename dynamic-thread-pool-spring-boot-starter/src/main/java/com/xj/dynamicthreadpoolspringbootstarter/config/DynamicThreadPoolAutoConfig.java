1a91092992d993a0ff01f31ce2d044565d114624
tiannoyi <tiannoyi@qq.com>
2024-05-21 10:03:18 +0800

    feat 配置文件

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/xj/dynamicthreadpoolspringbootstarter/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/xj/dynamicthreadpoolspringbootstarter/config/DynamicThreadPoolAutoConfig.java
@@ -1,12 +1,71 @@
 package com.xj.dynamicthreadpoolspringbootstarter.config;
 
+import com.alibaba.fastjson.JSON;
+import jodd.util.StringUtil;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author : xiongjun
  * @date : 2024/5/20 15:12
  * @description: 动态配置入口
  */
+@Configuration
+@EnableConfigurationProperties(DynamicThreadPoolAutoProperties.class)
+@EnableScheduling
 public class DynamicThreadPoolAutoConfig {
 
+    private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
 
+    @Bean("redissonClient")
+    public RedissonClient redissonClient(DynamicThreadPoolAutoProperties properties) {
+        Config config = new Config();
+        // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
+        config.setCodec(JsonJacksonCodec.INSTANCE);
+
+        config.useSingleServer()
+                .setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
+                .setPassword(properties.getPassword())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setIdleConnectionTimeout(properties.getIdleTimeout())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive());
+
+        RedissonClient redissonClient = Redisson.create(config);
+
+        logger.info("动态线程池，注册器（redis）链接初始化完成。{} {} {}", properties.getHost(), properties.getPoolSize(), !redissonClient.isShutdown());
+
+        return redissonClient;
+    }
+
+
+    @Bean("dynamicThreadPollService")
+    public String dynamicThreadPollService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap){
+        String applicationName = applicationContext.getEnvironment().getProperty("spring.application.name");
+        if (StringUtil.isNotBlank(applicationName)){
+            applicationName = "缺少的";
+            logger.warn("spring.application.name is null, use default applicationName:{}", applicationName);
+        }
+
+        logger.info("动态线程池{}", JSON.toJSONString(threadPoolExecutorMap.keySet()));
+
+        return new String();
+    }
 
 }
---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/xj/dynamicthreadpoolspringbootstarter/registry/RedisRegistry.java
@@ -0,0 +1,8 @@
+package com.xj.dynamicthreadpoolspringbootstarter.registry;
+
+/**
+ * @author : xiongjun
+ * @date : 2024/5/20 16:35
+ */
+public class RedisRegistry {
+}
